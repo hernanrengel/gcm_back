47ce4d71e64a8dbd23f907dd990f35cb1f129193
hrengel <hrengelc@gmail.com>
2015-03-16 15:21:40 -0400

    Init
    First Commit

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Database config variables
+ */
+define("DB_HOST", "localhost");
+define("DB_USER", "admin");
+define("DB_PASSWORD", "admin");
+define("DB_DATABASE", "gcm");
+
+/*
+ * Google API Key
+ */
+define("GOOGLE_API_KEY", "AIzaSyDU1hwO_t1NI1Bx4XIPqviok-zSFWCIruY"); // Place your Google API Key
+?>
